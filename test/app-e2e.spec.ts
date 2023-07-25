ebd01133096a40925175f2823d4c71205016548e
vishnucprasad <mail@vishnucprasad.in>
2023-07-25 12:46:54 +0530

    Setup e2e test for signin API

--- test/app-e2e.spec.ts
+++ test/app-e2e.spec.ts
@@ -3,12 +3,13 @@ import { Application } from 'express';
 import request from 'supertest';
 import { connection } from 'mongoose';
 import { container, serverConfig, serverErrorConfig } from '../src/config';
-import { RegisterDto } from '../src/auth/dto';
+import { RegisterDto, SigninDto } from '../src/auth/dto';
 
 import '../src/auth/auth.controller';
 
 let app: Application;
 let testServer: any;
+const store: Record<string, string> = {};
 
 beforeAll(async () => {
   const server = new InversifyExpressServer(container);
@@ -92,5 +93,66 @@ describe('POST /auth/register', () => {
     const body = JSON.stringify(response.body);
     expect(body).toContain(dto.name);
     expect(body).toContain(dto.email);
+  });
+});
+
+describe('POST /auth/signin', () => {
+  it('should throw an error if body not provided ', async () => {
+    const response = await request(app).post('/auth/signin');
+    expect(response.status).toBe(400);
+    expect(response.body['type']).toBe('Validation Error');
+  });
+
+  it('should throw an error if email is empty', async () => {
+    const dto: Omit<SigninDto, 'email'> = {
+      password: 'JohnDoe@123',
+    };
+
+    const response = await request(app).post('/auth/signin').send(dto);
+    expect(response.status).toBe(400);
+    expect(response.body['type']).toBe('Validation Error');
+  });
+
+  it('should throw an error if password is empty', async () => {
+    const dto: Omit<SigninDto, 'password'> = {
+      email: 'johndoe@example.com',
+    };
+
+    const response = await request(app).post('/auth/signin').send(dto);
+    expect(response.status).toBe(400);
+    expect(response.body['type']).toBe('Validation Error');
+  });
+
+  it('should throw an error if email is invalid', async () => {
+    const dto: SigninDto = {
+      email: 'invaliduser@invalid.com',
+      password: 'JohnDoe@123',
+    };
+
+    const response = await request(app).post('/auth/signin').send(dto);
+    expect(response.status).toBe(404);
+    expect(response.body['type']).toBe('Not found');
+  });
+
+  it('should throw an error if password is invalid', async () => {
+    const dto: SigninDto = {
+      email: 'johndoe@example.com',
+      password: 'invalid',
+    };
+
+    const response = await request(app).post('/auth/signin').send(dto);
+    expect(response.status).toBe(401);
+    expect(response.body['type']).toBe('Unauthorized');
+  });
+
+  it('should signin user', async () => {
+    const dto: SigninDto = {
+      email: 'johndoe@example.com',
+      password: 'JohnDoe@123',
+    };
+
+    const response = await request(app).post('/auth/signin').send(dto);
+    expect(response.status).toBe(200);
+    store['access_token'] = response.body['access_token'];
   });
 });
